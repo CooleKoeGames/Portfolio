0d0fc1383f9167dd0ef286142bd5a9ffa64cd2b5
CooleKoeGames <133784369+CooleKoeGames@users.noreply.github.com>
2025-03-20 17:13:29 +0100

    Add gitignore and formatted all the code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,59 +1,60 @@
 // Filters tabs
-const tabs = document.querySelectorAll("[data-target]"), tabsContents = document.querySelectorAll("[data-content]");
+const tabs = document.querySelectorAll('[data-target]'),
+  tabsContents = document.querySelectorAll('[data-content]');
 
 tabs.forEach(tab => {
-    tab.addEventListener("click", () => {
-        const target = document.querySelector(tab.dataset.target);
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target);
 
-        tabsContents.forEach(tc => {
-            tc.classList.remove("filters__active");
-        });
-        target.classList.add("filters__active");
-
-        tabs.forEach(t => {
-            t.classList.remove("filter-tab-active");
-        });
-        tab.classList.add("filter-tab-active");
+    tabsContents.forEach(tc => {
+      tc.classList.remove('filters__active');
     });
+    target.classList.add('filters__active');
+
+    tabs.forEach(t => {
+      t.classList.remove('filter-tab-active');
+    });
+    tab.classList.add('filter-tab-active');
+  });
 });
 
 // Dark light theme
-const themeButton = document.getElementById("theme-button");
-const darkTheme = "dark-theme";
-const iconTheme = "ri-sun-line";
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'ri-sun-line';
 
-const selectedTheme = localStorage.getItem("selected-theme");
-const selectedIcon = localStorage.getItem("selected-icon");
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
 
-const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? "dark" : "light";
-const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? "ri-moon-line" : "ri-sun-line";
+const getCurrentTheme = () => (document.body.classList.contains(darkTheme) ? 'dark' : 'light');
+const getCurrentIcon = () => (themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line');
 
 if (selectedIcon) {
-    document.body.classList[selectedTheme === "dark" ? "add" : "remove"](darkTheme);
-    themeButton.classList[selectedIcon === "ri-moon-line" ? "add" : "remove"](iconTheme);
-};
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+  themeButton.classList[selectedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme);
+}
 
-themeButton.addEventListener("click", () => {
-    document.body.classList.toggle(darkTheme);
-    themeButton.classList.toggle(iconTheme);
+themeButton.addEventListener('click', () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
 
-    localStorage.setItem("selected-theme", getCurrentTheme());
-    localStorage.setItem("selected-icon", getCurrentIcon());
-})
+  localStorage.setItem('selected-theme', getCurrentTheme());
+  localStorage.setItem('selected-icon', getCurrentIcon());
+});
 
 // Scroll reveal animation
-const sr = ScrollReveal ({
-    origin: "top",
-    distance: "60px",
-    duration: 2500,
-    delay: 400,
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
 });
 
 sr.reveal(`.profile__border`);
-sr.reveal(`.profile__name`, {delay: 500});
-sr.reveal(`.profile__profession`, {delay: 600});
-sr.reveal(`.profile__social`, {delay: 700});
-sr.reveal(`.profile__info-group`, {interval: 100,delay: 700});
-sr.reveal(`.profile__buttons`, {delay: 800});
-sr.reveal(`.filters__content`, {delay: 900});
-sr.reveal(`.filters`, {delay: 1000});
+sr.reveal(`.profile__name`, { delay: 500 });
+sr.reveal(`.profile__profession`, { delay: 600 });
+sr.reveal(`.profile__social`, { delay: 700 });
+sr.reveal(`.profile__info-group`, { interval: 100, delay: 700 });
+sr.reveal(`.profile__buttons`, { delay: 800 });
+sr.reveal(`.filters__content`, { delay: 900 });
+sr.reveal(`.filters`, { delay: 1000 });
